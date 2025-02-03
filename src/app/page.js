c43da639617dd4058585647b1263060830d586ba
Tapasvi Chhabhaiya <117344055+TTHPATEL@users.noreply.github.com>
2025-02-03 09:03:32 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -65,60 +65,68 @@ export default function Home() {
   }
 
   return (
-    <div style={{ margin: 120 }}>
-      <form action={Addscheduledmail} className="max-w-sm mx-auto">
-        <label
-          htmlFor="template"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Select Template ( Mailer )
-        </label>
-        <select
-          id="template"
-          name="template"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        >
-          {templist.map((tlist) => (
-            <option key={tlist.template_id}>{tlist.template_name}</option>
-          ))}
-        </select>
-        <br />
-        <label
-          htmlFor="recipient"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Select Recipient List
-        </label>
-        <select
-          id="recipient"
-          name="recipient"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        >
-          {categorylist.map((ulist) => (
-            <option key={ulist.categoryID}>{ulist.categoryName}</option>
-          ))}
-        </select>
-        <br />
-        <label
-          htmlFor="schedule"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Select Schedule
-        </label>
-        <input
-          type="datetime-local"
-          id="schedule"
-          name="schedule"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        ></input>
-        <br />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded center"
-        >
-          ADD SCHEDULE
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="flex justify-center w-full">
+        <h1 className="text-[2.6rem] font-bold text-center mt-16">
+          Make Mail Scheduler
+        </h1>
+      </div>
+
+      <div style={{ margin: 50 }}>
+        <form action={Addscheduledmail} className="max-w-sm mx-auto">
+          <label
+            htmlFor="template"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Select Template ( Mailer )
+          </label>
+          <select
+            id="template"
+            name="template"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            {templist.map((tlist) => (
+              <option key={tlist.template_id}>{tlist.template_name}</option>
+            ))}
+          </select>
+          <br />
+          <label
+            htmlFor="recipient"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Select Recipient List
+          </label>
+          <select
+            id="recipient"
+            name="recipient"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            {categorylist.map((ulist) => (
+              <option key={ulist.categoryID}>{ulist.categoryName}</option>
+            ))}
+          </select>
+          <br />
+          <label
+            htmlFor="schedule"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Select Schedule
+          </label>
+          <input
+            type="datetime-local"
+            id="schedule"
+            name="schedule"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          ></input>
+          <br />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded center"
+          >
+            ADD SCHEDULE
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
